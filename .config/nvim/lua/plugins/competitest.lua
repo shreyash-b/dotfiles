30d7d93615bf4af1d9cb5dbcde8bf12939d14005
Shreyash Bubane <bubaneshreyash3@gmail.com>
2024-12-02 00:30:46 +0530

    [nvim] add competites

---
+++ .config/nvim/lua/plugins/competitest.lua
@@ -0,0 +1,8 @@
+return {
+  "xeluxee/competitest.nvim",
+  dependencies = 'MunifTanjim/nui.nvim',
+  opts = {
+    -- testcases_directory = "/home/shreyash/.local/share/competitest",
+    testcases_use_single_file = true,
+  }
+}
