19107238eea52b4af607eb419d3391894b73d705
Shreyash Bubane <bubaneshreyash3@gmail.com>
2024-12-13 23:56:03 +0530

    add autoformat on save for lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,23 +1,38 @@
-local on_attach = function(_, buffer)
-  local map = function(lhs, rhs, desc)
-    local opts = { desc = desc, buffer = buffer }
+local on_attach = function(client, bufnr)
+  local bufmap = function(lhs, rhs, desc)
+    local opts = { desc = desc, buffer = bufnr }
     vim.keymap.set('n', lhs, rhs, opts)
   end
 
-  map('gD', vim.lsp.buf.declaration, "Goto Declaration")
-  map('gd', vim.lsp.buf.definition, "Goto Definition")
-  map('gt', vim.lsp.buf.type_definition, "Goto Type Definition")
-  map('gi', vim.lsp.buf.implementation, "Goto Implementation")
-  map('gr', vim.lsp.buf.references, "Show References")
-  map('gs', vim.lsp.buf.signature_help, "Signature help")
-  map('K', vim.lsp.buf.hover, "Toggle Documentation")
-  map('<leader>lw', vim.lsp.buf.document_symbol, "Document Symbols")
-  map('<leader>lW', vim.lsp.buf.workspace_symbol, "Workspace Symbols")
-  map('<leader>la', vim.lsp.buf.code_action, "Code Action")
-  map('<leader>lr', vim.lsp.buf.rename, "Rename Symbol")
-  map('<leader>lf', vim.lsp.buf.format, "Format Code")
-  map('<leader>li', vim.lsp.buf.incoming_calls, "Incoming Calls")
-  map('<leader>lh', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, "Toogle Inlay Hints")
+  bufmap('gD', vim.lsp.buf.declaration, "Goto Declaration")
+  bufmap('gd', vim.lsp.buf.definition, "Goto Definition")
+  bufmap('gt', vim.lsp.buf.type_definition, "Goto Type Definition")
+  bufmap('gi', vim.lsp.buf.implementation, "Goto Implementation")
+  bufmap('gr', vim.lsp.buf.references, "Show References")
+  bufmap('gs', vim.lsp.buf.signature_help, "Signature help")
+  bufmap('K', vim.lsp.buf.hover, "Toggle Documentation")
+  bufmap('<leader>lw', vim.lsp.buf.document_symbol, "Document Symbols")
+  bufmap('<leader>lW', vim.lsp.buf.workspace_symbol, "Workspace Symbols")
+  bufmap('<leader>la', vim.lsp.buf.code_action, "Code Action")
+  bufmap('<leader>lr', vim.lsp.buf.rename, "Rename Symbol")
+  bufmap('<leader>lf', vim.lsp.buf.format, "Format Code")
+  bufmap('<leader>li', vim.lsp.buf.incoming_calls, "Incoming Calls")
+  bufmap('<leader>lh', function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, "Toogle Inlay Hints")
+
+  if client.supports_method("textDocument/formatting") then
+    local augroup = vim.api.nvim_create_augroup("LspCommands", {});
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+
+    bufmap("<leader>qW", ":noautocmd w", "Write without formatting")
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format()
+      end
+    })
+  end
 end
 
 return {
