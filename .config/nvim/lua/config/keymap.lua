0caa69296515ea4eda4c20b757e5b3a40261f173
Shreyash Bubane <bubaneshreyash3@gmail.com>
2025-03-30 21:01:47 +0530

    add keymaps

---
+++ .config/nvim/lua/config/keymap.lua
@@ -0,0 +1,4 @@
+vim.keymap.set("n", "<leader>bd", "<cmd>bdelete<cr>", { desc = "Delete Buffer" })
+vim.keymap.set("n", "<leader>L", "<cmd>Lazy<cr>", { desc = "Lazy" })
+vim.keymap.set("n", "<leader>q", "<cmd>quitall<cr>", { desc = "Quit all windows" })
+vim.keymap.set("n", "<leader>w", "<cmd>write<cr>", { desc = "Write current buffer" })
