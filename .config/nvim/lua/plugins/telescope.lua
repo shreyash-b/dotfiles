cab3f73c4afa16a507c7c50a80a8ca7f04fc3c29
Shreyash Bubane <bubaneshreyash3@gmail.com>
2025-10-07 14:31:16 +0530

    [vim] update telescope binding

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -10,7 +10,7 @@ return {
     require("telescope").load_extension("ui-select")
 
     vim.keymap.set("n", "<leader>f", function() end, { desc = "Telescope" })
-    vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "File" })
+    vim.keymap.set("n", "<leader>ff", "<cmd>Telescope git_files<cr>", { desc = "File" })
     vim.keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { desc = "Buffer" })
     vim.keymap.set("n", "<leader>fk", "<cmd>Telescope keymaps<cr>", { desc = "Keymaps" })
     vim.keymap.set("n", "<leader>fl", "<cmd>Telescope live_grep<cr>", { desc = "Live Grep" })
